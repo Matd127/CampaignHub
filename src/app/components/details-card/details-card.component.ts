6a1c5b56fa2bdd0cc4e18412e2fb1f08ec838684
Matd127 <mateusz333555@gmail.com>
2024-03-18 00:40:06 +0100

    fix: Fixed issue with data loading in details

--- src/app/components/details-card/details-card.component.ts
+++ src/app/components/details-card/details-card.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 import { Campaign } from '../../../models/Campaign';
 import { ReadCampaigns } from '../../../store/campaign.action';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../shared/button/button.component';
 
@@ -16,18 +16,25 @@ import { ButtonComponent } from '../../shared/button/button.component';
   styleUrl: './details-card.component.scss',
 })
 export class DetailsCardComponent {
+  campaignId: number | undefined;
+
   campaign!: Observable<Campaign>;
 
   constructor(private store: Store<any>, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.store.dispatch(new ReadCampaigns());
-    this.campaign = this.store
-      .select('campaigns')
-      .pipe(
-        map((campaigns) =>
-          campaigns.find((campaign: Campaign) => campaign.id === 1)
-        )
-      );
+    this.route.params.subscribe((params) => {
+      this.campaignId = +params['id'];
+      this.store.dispatch(new ReadCampaigns());
+      this.campaign = this.store
+        .select('campaigns')
+        .pipe(
+          map((campaigns) =>
+            campaigns.find(
+              (campaign: Campaign) => campaign.id === this.campaignId
+            )
+          )
+        );
+    });
   }
 }
