5b6ebd4677eb3bbb40be2ad094886586600efcf6
Matd127 <mateusz333555@gmail.com>
2024-03-19 15:23:16 +0100

    chore: Add dummy data if user does not have any campaign

--- src/store/campaign.reducer.ts
+++ src/store/campaign.reducer.ts
@@ -5,11 +5,36 @@ import { Campaign } from '../models/Campaign';
 const dataFromLocalStorage = localStorage?.getItem('campaigns') || '';
 const existingData = dataFromLocalStorage && JSON.parse(dataFromLocalStorage);
 
-const campaigns: Campaign[] = existingData;
+const campaigns: Campaign[] = existingData
+  ? existingData
+  : [
+      {
+        id: 1010858033631,
+        name: 'Summer Sale',
+        keywords: ['summer', 'sale', 'discount'],
+        bidAmount: 0.5,
+        campaignFund: 100,
+        status: false,
+        town: 'Krakow',
+        radius: 10,
+      },
+      {
+        id: 1010858033632,
+        name: 'Holiday Campaign',
+        keywords: ['holiday', 'gift', 'promotion'],
+        bidAmount: 0.7,
+        campaignFund: 200,
+        status: false,
+        town: 'Warsaw',
+        radius: 15,
+      },
+    ];
 
 export function campaignReducer(state = campaigns, action: Action) {
   switch (action.type) {
     case ActionTypes.ReadCampaigns:
+      console.log(existingData ? '1' : '0');
+
       return state;
 
     case ActionTypes.CreateCampaign:
